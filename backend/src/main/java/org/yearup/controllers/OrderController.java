644d8670eff711bf6f6960ca09eabd2b50ca9e12
luisvasquezgonzalez <luicho.vasquez44@gmail.com>
2026-06-26 01:00:37 -0400

    Fixed the request mapping name mismatch bug

--- backend/src/main/java/org/yearup/controllers/OrderController.java
+++ backend/src/main/java/org/yearup/controllers/OrderController.java
@@ -1,7 +1,5 @@
 package org.yearup.controllers;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,7 +13,7 @@ import org.yearup.service.UserService;
 import java.security.Principal;
 
 @RestController
-@RequestMapping("/Order")
+@RequestMapping("/orders")
 @CrossOrigin
 @PreAuthorize("hasRole('ROLE_USER')")
 public class OrderController {
